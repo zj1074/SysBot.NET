89db2e74c99dd3d3398b7bbf611269b947fffe94
Easy World <easy_world_118@Hotmail.com>
2023-01-30 14:06:17 +0800

    add missing parameter

--- SysBot.Pokemon.Dodo/DodoTradeNotifier.cs
+++ SysBot.Pokemon.Dodo/DodoTradeNotifier.cs
@@ -36,7 +36,8 @@ namespace SysBot.Pokemon.Dodo
                 string tid = regex.Match(message).Groups[1].ToString();
                 regex = new Regex("SID: (\\d+)");
                 string sid = regex.Match(message).Groups[1].ToString();
-                DodoBot<T>.SendPersonalMessage(info.Trainer.ID.ToString(), $"找到你了，你的SID7:{sid},TID7:{tid}");
+                DodoBot<T>.SendPersonalMessage(info.Trainer.ID.ToString(), $"找到你了，你的SID7:{sid},TID7:{tid}",
+                    info.Context.GetValueOrDefault("islandSourceId", ""));
             }
         }
 
